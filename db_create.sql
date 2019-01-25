f827854ce7d3d4e2c6f2a36c87bd992a6d562321
TimaVolgin <44920870+TimaVolgin@users.noreply.github.com>
2019-01-25 12:57:48 +0300

    Some fixes

--- db_create.sql
+++ db_create.sql
@@ -2,46 +2,46 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE occupation (
-    id serial PRIMARY KEY,
-    name varchar (50) NOT NULL
+    occupation_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE employer (
-    id serial PRIMARY KEY,
-    name varchar (50) NOT NULL
+    employer_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE vacancy (
-    id serial PRIMARY KEY,
-    text varchar (50) NOT NULL,
-    employer_id integer REFERENCES employer(id),
-    occupation_id integer REFERENCES occupation(id),
-    skill integer NOT NULL
+    vacancy_id SERIAL PRIMARY KEY,
+    text VARCHAR(50) NOT NULL,
+    employer_id INTEGER REFERENCES employer(employer_id),
+    occupation_id INTEGER REFERENCES occupation(occupation_id),
+    experience INTEGER NOT NULL
 );
 
-CREATE TABLE worker (
-    id serial PRIMARY KEY,
-    name varchar (50) NOT NULL
+CREATE TABLE applicant (
+    applicant_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE resume (
-    id serial PRIMARY KEY,
-    worker_id integer REFERENCES worker(id),
-    occupation_id integer REFERENCES occupation(id),
-    text varchar (50) NOT NULL,
-    skill integer NOT NULL
+    resume_id SERIAL PRIMARY KEY,
+    applicant_id INTEGER REFERENCES applicant(applicant_id),
+    occupation_id INTEGER REFERENCES occupation(occupation_id),
+    text VARCHAR(50) NOT NULL,
+    experience INTEGER NOT NULL
 );
 
 CREATE TABLE application (
-    id serial PRIMARY KEY,
-    vacancy_id integer REFERENCES vacancy(id),
-    worker_id integer REFERENCES worker(id)
+    application_id SERIAL PRIMARY KEY,
+    vacancy_id INTEGER REFERENCES vacancy(vacancy_id),
+    applicant_id INTEGER REFERENCES applicant(applicant_id)
 );
 
 CREATE TABLE invite (
-    id serial PRIMARY KEY,
-    vacancy_id integer REFERENCES vacancy(id),
-    worker_id integer REFERENCES worker(id)
+    invite_id SERIAL PRIMARY KEY,
+    vacancy_id INTEGER REFERENCES vacancy(vacancy_id),
+    applicant_id INTEGER REFERENCES applicant(applicant_id)
 );
 
--- db_fill.sql
+++ db_fill.sql
@@ -11,32 +11,32 @@ INSERT INTO employer (name) VALUES
 ('Tinkoff'),
 ('Facebook');
 
-INSERT INTO vacancy (text, employer_id, occupation_id, skill) VALUES
+INSERT INTO vacancy (text, employer_id, occupation_id, experience) VALUES
 ('Mega developer', 4, 1, 90),
 ('Bash lector', 1, 3, 95),
 ('Java developer', 2, 1, 95),
 ('JS developer', 3, 4, 90),
 ('Need HR', 1, 2, 70);
 
-INSERT INTO worker (name) VALUES
+INSERT INTO applicant (name) VALUES
 ('Vasily'),
 ('Peter'),
 ('Kolya'),
 ('Innokenty');
 
-INSERT INTO resume (worker_id, occupation_id, text, skill) VALUES
+INSERT INTO resume (applicant_id, occupation_id, text, experience) VALUES
 (1, 1, 'I`m super!', 90),
 (2, 3, 'I`m super lector!', 80),
 (3, 4, 'I`m super frontender!', 65),
 (4, 5, 'I`m super Java dev!', 98);
 
-INSERT INTO application (vacancy_id, worker_id) VALUES
+INSERT INTO application (vacancy_id, applicant_id) VALUES
 (1, 1),
 (2, 2),
 (3, 3),
 (4, 4);
 
-INSERT INTO invite (vacancy_id, worker_id) VALUES
+INSERT INTO invite (vacancy_id, applicant_id) VALUES
 (1, 2),
 (2, 1),
 (3, 4),
