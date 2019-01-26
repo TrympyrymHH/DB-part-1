a5554cf40fd9ef9f704ee078fb9a89d410d1340c
TimaVolgin <44920870+TimaVolgin@users.noreply.github.com>
2019-01-27 02:00:07 +0300

    Add usecases

--- db_fill.sql
+++ db_fill.sql
@@ -12,29 +12,32 @@ INSERT INTO employer (name) VALUES
 ('Facebook');
 
 INSERT INTO vacancy (text, employer_id, occupation_id, experience) VALUES
-('Mega developer', 4, 1, 90),
-('Bash lector', 1, 3, 95),
-('Java developer', 2, 1, 95),
-('JS developer', 3, 4, 90),
-('Need HR', 1, 2, 70);
+('Bash lector',     1, 3, 95),
+('Mega developer',  4, 1, 90),
+('Java developer',  2, 1, 95),
+('JS developer',    3, 4, 90),
+('Need HR',         1, 2, 70);
 
 INSERT INTO applicant (name) VALUES
 ('Vasily'),
 ('Peter'),
 ('Kolya'),
-('Innokenty');
+('Innokenty'),
+('Maria');
 
 INSERT INTO resume (applicant_id, occupation_id, text, experience) VALUES
-(1, 1, 'I`m super!', 90),
-(2, 3, 'I`m super lector!', 80),
+(1, 1, 'I`m super!',            96),
+(2, 3, 'I`m super lector!',     96),
 (3, 4, 'I`m super frontender!', 65),
-(4, 5, 'I`m super Java dev!', 98);
+(4, 1, 'I`m super Java dev!',   98),
+(5, 2, 'I`m super HR!',         71);
 
 INSERT INTO application (vacancy_id, applicant_id) VALUES
-(1, 1),
+(3, 1),
 (2, 2),
 (3, 3),
-(4, 4);
+(4, 4),
+(1, 2);
 
 INSERT INTO invite (vacancy_id, applicant_id) VALUES
 (1, 2),
---
+++ usecases.sql
@@ -0,0 +1,40 @@
+﻿-- работодатель ищет людей, подходящих на конкретную вакансию
+SELECT distinct a.applicant_id, a.name 
+FROM applicant a
+JOIN resume r USING (applicant_id)
+JOIN vacancy v ON (v.occupation_id = r.occupation_id AND r.experience >= v.experience AND v.vacancy_id = 3);
+
+-- работодатель ищет людей, подходящих на конкретную вакансию, но еще не подавщих на нее
+SELECT distinct a.applicant_id, a.name 
+FROM applicant a
+JOIN resume r USING (applicant_id)
+JOIN vacancy v ON (v.vacancy_id = 3 AND v.occupation_id = r.occupation_id AND r.experience >= v.experience)
+WHERE (a.applicant_id NOT IN (
+		SELECT (a.applicant_id)
+		FROM applicant a
+		JOIN application app ON (app.vacancy_id = 3 AND a.applicant_id = app.applicant_id)
+	)	
+);
+
+-- работодатель ищет людей, подходящих на любую свою вакансию
+SELECT distinct a.applicant_id, a.name
+FROM applicant a
+JOIN resume r USING (applicant_id)
+JOIN vacancy v ON (v.employer_id = 1 AND v.occupation_id = r.occupation_id AND r.experience >= v.experience);
+
+-- работодатель приглашает всех подходящих людей, подавших на его вакансии, на собеседование
+INSERT INTO invite (vacancy_id, applicant_id)
+SELECT distinct v.vacancy_id, a.applicant_id
+FROM applicant a
+JOIN application app USING (applicant_id)
+JOIN vacancy v ON (v.employer_id = 1 AND v.vacancy_id = app.vacancy_id)
+JOIN resume r ON (a.applicant_id = r.applicant_id AND r.occupation_id = v.occupation_id AND r.experience >= v.experience);
+
+-- соискатель выбирает все подходящие по опыту вакансии в конкретной области
+SELECT v.text, v.experience, e.name
+FROM vacancy v
+JOIN employer e USING (employer_id)
+where v.occupation_id = 1 and v.experience <= 90
+
+-- соискатель подает на вакансию
+INSERT INTO application (vacancy_id, applicant_id) VALUES (3, 1);
